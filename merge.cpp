4febbac7141ea8dfb05c321c73b06792696b0ce8
Vidul <vidulkumar@gmail.com>
2019-06-13 19:16:22 +0530

    updated merge

--- merge.cpp
+++ merge.cpp
@@ -4,38 +4,27 @@ void Solution::merge(vector<int> &A, vector<int> &B) {
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     int bindex = 0,aindex=A.size();
-    int sz = A.size()+B.size();
-    A.resize(sz);
-    for(int i=0;i<sz,bindex<B.size();i++)
+    
+    for(int i=0;i<A.size(),bindex<B.size();i++)
     {   
         if(A[i]>B[bindex])
         {
-            //cout<<"case1"<<endl;
-            for(int j=sz-2;j>=i;j--)
-            {
-                A[j+1]=A[j];
-            }
-            A[i]=B[bindex];
+            
+            A.insert(A.begin()+i,B[bindex]);
             bindex++;
         }
         else if(B[bindex]>=A[aindex-1])
         {
             //cout<<"case2"<<endl;
             break;
-        }/*
-        for(int k=0;k<sz;k++)
-        {
-            cout<<A[k]<<" ";
         }
-        cout<<endl;*/
+
     }
-    aindex= aindex+bindex;
-    while(aindex<A.size()&&bindex<B.size())
+
+    while(bindex<B.size())
     {
-        A[aindex] = B[bindex];
-        aindex++;
+        A.push_back(B[bindex]);
         bindex++;
-        
     }
     
 }
